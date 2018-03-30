61c7d2571ece5e915ea58c4277c1b12f76f4a58a
Shyam Saraswati <shyam.saraswati@gmail.com>
2018-03-30 13:04:25 +1100

    fonts

--- app.py
+++ app.py
@@ -6,35 +6,62 @@ from json import load
 from urllib2 import urlopen
 hostname = socket.gethostname() 
 test_file = 'sample-out.jpg'
+image_width, image_height = (250, 122)
+
+font_big = ImageFont.truetype('./enhanced_dot_digital-7.ttf', 20)
+font_medium = ImageFont.truetype('./runescape_uf.ttf', 16)
+font_small = ImageFont.truetype('./Super_Mario_World_Text_Box.ttf', 7)
+
+
+def newBlankImage():
+    image = Image.new('1', (image_width, image_height), 255) 
+    draw = ImageDraw.Draw(image)
+    return image, draw
+
+def draw_image(image_to_draw):
+    if hostname == "raspberrypi":
+        print "Drawing Frame"
+        epd.set_frame_memory(image_to_draw.rotate(90), 0, 0)
+        epd.display_frame()
+    else:
+        print test_file
+        image_to_draw.save(test_file)
 
 if hostname == "raspberrypi":
     import epd2in13
     epd = epd2in13.EPD()
-    epd.init(epd.lut_full_update)
+    epd.init(epd.lut_partial_update)
     
 
-# ip_public = load(urlopen('http://jsonip.com'))['ip']
+ip_public = load(urlopen('http://jsonip.com'))['ip']
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ip = s.getsockname()[0]
 s.close()
 
-# For simplicity, the arguments are explicit numerical coordinates
-image = Image.new('1', (128, 250), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('./FreeMonoBold.ttf', 10)
-image_width, image_height = (96, 32)
+# Draw device Ip
+image, draw = newBlankImage()
+ip_output = '{0:10s} ...Starting'.format(ip)
+draw.text((5, 14), ip_output, font=font_big, fill=0)
+draw_image(image)
+time.sleep(2)
+############
+
 while True:
-    print 'Ping...Pong?'
+    image, draw = newBlankImage()
+    
+
+    ip_output = '{0:10s}'.format(ip_public)   
+
     r = pyping.ping('google.com')
-    output = "IP: " + ip + "Public IP: " + "sdf" + "ms:" + r.avg_rtt
+
+    ping_output = '       {0:10.0f}ms'.format(float(r.avg_rtt))
 
     draw.rectangle((0, 0, image_width, image_height), fill = 255)
-    draw.text((30, 14), output, font=font, fill=0)
-    if hostname == "pynet":
-        epd.display_frame()
-    else:
-        print test_file
-    image.save(test_file)
+    draw.text((5, 14), ip_output, font=font_medium, fill=0)
+    draw.text((5, 90), ping_output, font=font_big, fill=0)
 
-    time.sleep(5)
+
+    draw_image(image)
+
+
